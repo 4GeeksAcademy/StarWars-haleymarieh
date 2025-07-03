4a41eb5e5bc0e7471f56e25070dff861f6b7612f
Haley <haleymariehernandez10@gmail.com>
2025-07-03 01:06:31 +0000

    first push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,36 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Peoplecard } from "../components/bootstrapcards.jsx";
+import React, {useEffect, useState} from "react";
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+
+useEffect(() => {
+		getpeopleinfo()
+	}, []);
+
+  const getpeopleinfo = () => {
+	fetch('https://www.swapi.tech/api/people')
+		.then(response => {
+				if (!response.ok) throw new Error(response.statusText);
+				return response.json();
+			})
+		.then(data => {
+				setPeople (data.results)
+			})
+		.catch(error => console.error('Error fetching people info:', error));
+		}
+
+		
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			{people.map(
+				(person) => <Peoplecard character={person}/>
+			)}	
 		</div>
 	);
 }; 
--- src/routes.jsx
+++ src/routes.jsx
@@ -9,6 +9,7 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+import { Details } from "./pages/Details";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,7 +24,8 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
+        <Route path="/single/:theId" element={ <Single />} />
+        <Route path="/details/:theId" element={ <Details />} />  
         <Route path="/demo" element={<Demo />} />
       </Route>
     )
